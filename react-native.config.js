9c1fe33d02cea7e961a5fbbcf73f5bdf93fa4aae
kagol <vits21731a0584@gmail.com>
2025-12-06 19:29:47 +0530

    Fix react-native-sqlite-storage dependency - downgrade to v5.0.0 and manual linking

--- react-native.config.js
+++ react-native.config.js
@@ -3,11 +3,8 @@ module.exports = {
     'react-native-sqlite-storage': {
       platforms: {
         android: {
-          sourceDir: '../node_modules/react-native-sqlite-storage/src/android',
-          packageImportPath: 'android.database.sqlite.SQLiteDatabase',
-        },
-        ios: {
-          podspecPath: '../node_modules/react-native-sqlite-storage/react-native-sqlite-storage.podspec',
+          sourceDir: '../node_modules/react-native-sqlite-storage/platforms/android',
+          packageImportPath: 'org.pgsqlite.SQLitePluginPackage',
         },
       },
     },
